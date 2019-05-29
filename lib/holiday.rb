8a3858f8eeb1a8d45b4be49b075ed71444b44d39
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-29 16:09:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,5 +79,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   result = []
+  holiday_hash.each do |season, data|
+
+  end
 
 end
