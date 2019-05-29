a6492690785e54842d0294367050d0c4bd5482dc
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-29 16:10:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, value|
       value.each do |supply|
-
+        if supply == "BBQ"
+          result << holiday
       end
     end
   end
