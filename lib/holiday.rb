58c73ad3ee8896e1d7d42b602fa71b37f80aaf51
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-29 04:23:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
 
       puts " #{total_holidays.join(", ")}: #{supply.join(", ")}"
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
